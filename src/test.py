0cb626133412ca103466ef502adadde8594f2cd4
zhangli <1743847476@qq.com>
2023-09-07 02:18:39 +0800

    add test

--- src/test.py
+++ src/test.py
@@ -3,3 +3,16 @@ a = 1
 b = 2
 c = a + b
 print(c)
+print("2")
+a = 1
+b = 2
+for i in range(10):
+    print(1)
+def func():
+    """
+        test
+    """
+    print("1")
+func()
+a = "fa"
+print(a.center)
